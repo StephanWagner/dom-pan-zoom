aba9e38a589442e65756825cd0951308e613ff62
Stephan Wagner <stephanwagner.me@gmail.com>
2026-01-03 12:09:36 +0930

    Minor formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,45 @@
-import resolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
-const name = "domPanZoom";
+const name = 'domPanZoom';
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: [
     // ESM
     {
-      file: "dist/index.js",
-      format: "es",
-      sourcemap: true,
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true
     },
     // CommonJS
     {
-      file: "dist/index.cjs",
-      format: "cjs",
+      file: 'dist/index.cjs',
+      format: 'cjs',
       sourcemap: true,
-      exports: "default",
+      exports: 'default'
     },
     // UMD (browser global)
     {
-      file: "dist/dom-pan-zoom.umd.js",
-      format: "umd",
+      file: 'dist/dom-pan-zoom.umd.js',
+      format: 'umd',
       name,
-      sourcemap: true,
+      sourcemap: true
     },
     {
-      file: "dist/dom-pan-zoom.umd.min.js",
-      format: "umd",
+      file: 'dist/dom-pan-zoom.umd.min.js',
+      format: 'umd',
       name,
       plugins: [
         terser({
           compress: true,
           mangle: true,
           format: {
-            comments: false,
-          },
-        }),
-      ],
-    },
+            comments: false
+          }
+        })
+      ]
+    }
   ],
-  plugins: [resolve()],
+  plugins: [resolve()]
 };
